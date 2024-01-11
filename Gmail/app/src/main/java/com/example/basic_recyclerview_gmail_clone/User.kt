d44558b570d344b2c183043f52a9bc90fdce1319
Van-Dong <dong.nv204724@sis.hust.edu.vn>
2024-01-12 00:08:58 +0700

    add_source_code

---
+++ Gmail/app/src/main/java/com/example/basic_recyclerview_gmail_clone/User.kt
@@ -0,0 +1,10 @@
+package com.example.basic_recyclerview_gmail_clone
+
+data class User(
+    var name: String,
+    var lastMessage: String,
+    var lastMsgtime: String,
+    var phoneNo: String,
+    var country: String,
+    var imageId: Int
+)
